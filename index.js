5da322851b4e39d08ea54a128441590492059fd4
Vinayaka Patil <vinnu_313@yahoo.com>
2015-09-14 18:32:04 +0530

    index.html added.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/cool', function(request, response) {
-  response.send(cool());
+  response.render('public/index.html');
 });
 
 app.listen(app.get('port'), function() {
