7a75d9110f8938e5510ed004797dc00b04d88434
Vinayaka Patil <vinnu_313@yahoo.com>
2015-09-18 18:42:49 +0530

    Reordered script dependencies.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,6 +4,7 @@ angular.module('shippableApp.controllers', ['shippableApp.services'])
         .controller('AppCtrl', ['$q', '$scope', 'ShippableService', function ($q, $scope, ShippableService) {
                 $scope.appName = "Shippable screening test ";
                 $scope.showIssues = function () {
+                    $scope.results = false;
                     if ($scope.repoName.indexOf('github.com') === -1) {
                         alert("Not a github repo");
                     } else if ($scope.repoName.split('/').length !== 5) {
@@ -12,16 +13,13 @@ angular.module('shippableApp.controllers', ['shippableApp.services'])
                         var ar = $scope.repoName.split('/');
                         ShippableService.fetchIssues(ar[3], ar[4], 1).then(function (response) {
                             var iterations = Math.ceil(response.data.total_count / 100);
-                            console.log(iterations);
                             $scope.issues = response.data.items;
                             var promises = [];
                             for(var i = 1; i < iterations; ++i){
                                 promises.push(ShippableService.fetchIssues(ar[3], ar[4], i+1));
                             }
-                            console.log(promises);
                             if(promises.length){
                                 $q.all(promises).then(function(data){
-                                    console.log(data);
                                     for(i = 0; i< iterations-1; ++i){
                                         console.log(data[i].data.items);
                                         $scope.issues = $scope.issues.concat(data[i].data.items);
@@ -30,6 +28,7 @@ angular.module('shippableApp.controllers', ['shippableApp.services'])
                                     $scope.last24Hours = ShippableService.filter24Hours($scope.issues);
                                     $scope.last7Days = ShippableService.filterLast7Days($scope.issues);
                                     $scope.moreThan7Days = ShippableService.filterMoreThan7Days($scope.issues);
+                                    $scope.results = true;
                                 }, function(error){
                                     console.error(error);
                                 });    
@@ -37,6 +36,7 @@ angular.module('shippableApp.controllers', ['shippableApp.services'])
                                 $scope.last24Hours = ShippableService.filter24Hours($scope.issues);
                                 $scope.last7Days = ShippableService.filterLast7Days($scope.issues);
                                 $scope.moreThan7Days = ShippableService.filterMoreThan7Days($scope.issues);
+                                $scope.results = true;
                             }
                         }, function (response) {
                             console.error(response);
